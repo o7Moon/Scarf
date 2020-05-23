617e11a6e4cbe771efeb48a87c65e1b2ac9ee671
AwesomeMario128 / Tran Fox <22718078+Tran-Foxxo@users.noreply.github.com>
2020-05-23 09:11:52 -0400

    Trying to fix auto updating :p

--- src/Program.cs
+++ src/Program.cs
@@ -36,7 +36,7 @@ namespace linerider
 #endif
         public static string BinariesFolder = "bin";
         public readonly static CultureInfo Culture = new CultureInfo("en-US");
-        public static string Version = "1.04 - EXPERIMENTAL";
+        public static string Version = "1.04-EXPERIMENTAL";
         public static string TestVersion = "";
         public static string NewVersion = null;
         public static readonly string WindowTitle = "Line Rider: Advanced (Tran) " + Version + TestVersion;
