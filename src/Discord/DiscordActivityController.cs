c0a7b30450d5d0b0d40ac978c7c69791ad8f3705
AwesomeMario128 / Tran Fox <22718078+Tran-Foxxo@users.noreply.github.com>
2020-05-29 11:02:34 -0400

    Create file for Discord Game SDK (not used rn)

---
+++ src/Discord/DiscordActivityController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+
+//TODO: Finish a better Game SDK implementation 
+
+namespace linerider
+{
+    class DiscordActivityController
+    {
+        public Discord.Discord discord;
+
+        public void initDiscord()
+        {
+            discord = new Discord.Discord(506953593945980933, (UInt64)Discord.CreateFlags.NoRequireDiscord); //Create discord for game sdk activity
+        }
+
+    }
+}
