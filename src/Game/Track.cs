e905b39111e1244e2906cbb1d305409d47f411a1
MoonlitJolteon <rockhillnathan@gmail.com>
2020-09-04 18:04:43 -0400

    Id counter is important ;-;

--- src/Game/Track.cs
+++ src/Game/Track.cs
@@ -71,6 +71,8 @@ namespace linerider
         public float YGravity = 1; //default gravity
         public float XGravity = 0; //default gravity
         public double GravityWellSize = 10; //Default Gravity Well Size
+
+        internal int _idcounter;
         public Track()
         {
             GenerateBones();
@@ -127,7 +129,12 @@ namespace linerider
             }
             else
             {
-                line.ID = Lines.Count > 0 ? Lines.Max() + 1 : 1;
+                if (line.ID == GameLine.UninitializedID)
+                    line.ID = Lines.Count > 0 ? Lines.Max() + 1 : 0;
+                else if (line.ID >= _idcounter)
+                {
+                    _idcounter = line.ID + 1;
+                }
             }
             switch (line.Type)
             {
