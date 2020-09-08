73f35415907930cda3475d8254dea96eae2ddf87
AwesomeMario128 / Tran Fox <22718078+Tran-Foxxo@users.noreply.github.com>
2020-09-08 15:30:46 -0400

    Add length, angle and AGW options to the line window along with some restructuring to the code itself

--- src/UI/Dialogs/LineWindow.cs
+++ src/UI/Dialogs/LineWindow.cs
@@ -22,10 +22,15 @@ namespace linerider.UI
         private const string DefaultTitle = "Line Properties";
         private bool closing = false;
 
+        private NumberProperty length;
+        private NumberProperty angleProp;
+
         private NumberProperty multiplier;
         private NumberProperty multilines;
         private CheckProperty accelinverse;
 
+        private CheckProperty leftAGW;
+        private CheckProperty rightAGW;
         public LineWindow(GameCanvas parent, Editor editor, GameLine line) : base(parent, editor)
         {
             _ownerline = line;
@@ -36,7 +41,7 @@ namespace linerider.UI
             _proptree = new PropertyTree(this)
             {
                 Width = 220,
-                Height = 200
+                Height = 250
             };
             _proptree.Dock = Dock.Top;
             MakeModal(true);
@@ -97,6 +102,7 @@ namespace linerider.UI
         private void Setup()
         {
             SetupLineRightClickOptions(_proptree);
+            
             Panel bottom = new Panel(this)
             {
                 Dock = Dock.Bottom,
@@ -127,132 +133,196 @@ namespace linerider.UI
                 Close();
             };
         }
-        private void SetupLineRightClickOptions(PropertyTree tree)
+
+        private void SetupSceneryOptions(PropertyTree tree, PropertyTable lineProp)
         {
-            if (_ownerline is StandardLine)
+            var width = new NumberProperty(lineProp)
             {
-                int currentMultiplier = 0;
-                bool inv = false;
-                if (_ownerline is RedLine red)
+                Min = 0.1,
+                Max = 25.5,
+                NumberValue = _ownerline.Width,
+            };
+            width.ValueChanged += (o, e) =>
+            {
+                ChangeWidth(width.NumberValue);
+            };
+            lineProp.Add("Width", width);
+        }
+
+        private void SetupBlueAndRedOptions(PropertyTree tree)
+        {
+            int currentMultiplier = 0;
+            bool inv = false;
+            StandardLine.Ext lineEXT = ((StandardLine)_ownerline).Extension;
+            bool leftEXT = ((StandardLine)_ownerline).Extension == StandardLine.Ext.Both || ((StandardLine)_ownerline).Extension == StandardLine.Ext.Left;
+            bool rightEXT = ((StandardLine)_ownerline).Extension == StandardLine.Ext.Both || ((StandardLine)_ownerline).Extension == StandardLine.Ext.Right;
+
+
+            if (_ownerline is RedLine red)
+            {
+                currentMultiplier = red.Multiplier;
+                inv = red.inv;
+            }
+
+            var table = tree.Add("Acceleration", 120);
+            multiplier = new NumberProperty(table)
+            {
+                Min = -255,
+                Max = 255,
+                NumberValue = (inv ? -currentMultiplier : currentMultiplier),
+                OnlyWholeNumbers = true,
+            };
+            multiplier.ValueChanged += (o, e) =>
+            {
+                if (multiplier.NumberValue < 0)
                 {
-                    currentMultiplier = red.Multiplier;
-                    inv = red.inv;
+                    accelinverse.IsChecked = true;
+                }
+                else
+                {
+                    accelinverse.IsChecked = false;
+                }
+                ChangeMultiplier((int)Math.Abs(multiplier.NumberValue));
+            };
+            table.Add("Multiplier", multiplier);
+            multilines = new NumberProperty(table)
+            {
+                Min = 1,
+                Max = 9999,
+                OnlyWholeNumbers = true,
+            };
+            multilines.NumberValue = GetMultiLines(true).Count;
+            multilines.ValueChanged += (o, e) =>
+            {
+                Multiline((int)multilines.NumberValue);
+            };
+            table.Add("Multilines", multilines);
+
+            accelinverse = GwenHelper.AddPropertyCheckbox(
+                table,
+                "Inverse",
+                inv
+                );
+            accelinverse.ValueChanged += (o, e) =>
+            {
+                if (accelinverse.IsChecked)
+                {
+                    if (multiplier.NumberValue > 0) { multiplier.NumberValue = -multiplier.NumberValue; }
+                }
+                else
+                {
+                    multiplier.NumberValue = Math.Abs(multiplier.NumberValue);
                 }
 
-                var table = tree.Add("Acceleration", 120);
-                multiplier = new NumberProperty(table)
+                using (var trk = _editor.CreateTrackWriter())
                 {
-                    Min = -255,
-                    Max = 255,
-                    NumberValue = (inv ? -currentMultiplier : currentMultiplier),
-                    OnlyWholeNumbers = true,
-                };
-                multiplier.ValueChanged += (o, e) =>
-                {
-                    if (multiplier.NumberValue < 0)
+                    var multi = GetMultiLines(false);
+                    foreach (var l in multi)
                     {
-                        accelinverse.IsChecked = true;
-                    }
-                    else
-                    {
-                        accelinverse.IsChecked = false;
-                    }
-                    ChangeMultiplier((int)Math.Abs(multiplier.NumberValue));
-                };
-                table.Add("Multiplier", multiplier);
-                multilines = new NumberProperty(table)
-                {
-                    Min = 1,
-                    Max = 9999,
-                    OnlyWholeNumbers = true,
-                };
-                multilines.NumberValue = GetMultiLines(true).Count;
-                multilines.ValueChanged += (o, e) =>
-                {
-                    Multiline((int)multilines.NumberValue);
-                };
-                table.Add("Multilines", multilines);
-
-                accelinverse = GwenHelper.AddPropertyCheckbox(
-                    table,
-                    "Inverse",
-                    inv
-                    );
-                accelinverse.ValueChanged += (o, e) =>
-                {
-                    if (accelinverse.IsChecked)
-                    {
-                        if (multiplier.NumberValue > 0) { multiplier.NumberValue = -multiplier.NumberValue; }
-                    }
-                    else
-                    {
-                        multiplier.NumberValue = Math.Abs(multiplier.NumberValue);
+                        var cpy = (StandardLine)l.Clone();
+                        cpy.Position = l.Position2;
+                        cpy.Position2 = l.Position;
+                        cpy.inv = accelinverse.IsChecked;
+                        UpdateLine(trk, l, cpy);
                     }
 
-                    using (var trk = _editor.CreateTrackWriter())
-                    {
-                        var multi = GetMultiLines(false);
-                        foreach (var l in multi)
-                        {
-                            var cpy = (StandardLine)l.Clone();
-                            cpy.Position = l.Position2;
-                            cpy.Position2 = l.Position;
-                            cpy.inv = accelinverse.IsChecked;
-                            UpdateLine(trk, l, cpy);
-                        }
+                    var owner = (StandardLine)_ownerline.Clone();
+                    owner.Position = _ownerline.Position2;
+                    owner.Position2 = _ownerline.Position;
+                    owner.inv = accelinverse.IsChecked;
+                    UpdateOwnerLine(trk, owner);
+                }
+            };
 
-                        var owner = (StandardLine)_ownerline.Clone();
-                        owner.Position = _ownerline.Position2;
-                        owner.Position2 = _ownerline.Position;
-                        owner.inv = accelinverse.IsChecked;
-                        UpdateOwnerLine(trk, owner);
-                    }
-                };
-
-                multiplier.ValueChanged += (o, e) =>
-                {
-                    int val = (int)multiplier.NumberValue;
-                    if (val == 0)
-                    {
-                        accelinverse.Disable();
-                    }
-                    else
-                    {
-                        accelinverse.Enable();
-                    }
-                };
-
-                if ((int)multiplier.NumberValue == 0)
+            multiplier.ValueChanged += (o, e) =>
+            {
+                int val = (int)multiplier.NumberValue;
+                if (val == 0)
                 {
                     accelinverse.Disable();
                 }
+                else
+                {
+                    accelinverse.Enable();
+                }
+            };
+
+            if ((int)multiplier.NumberValue == 0)
+            {
+                accelinverse.Disable();
+            }
+
+            table = tree.Add("Anti-Gravity Wells", 120);
+            leftAGW = GwenHelper.AddPropertyCheckbox(table, "Left AGW", leftEXT);
+            leftAGW.ValueChanged += (o, e) =>
+            {
+                UpdateAGWs(leftAGW.IsChecked, rightAGW.IsChecked);
+            };
+            rightAGW = GwenHelper.AddPropertyCheckbox(table, "Right AGW", rightEXT);
+            rightAGW.ValueChanged += (o, e) =>
+            {
+                UpdateAGWs(leftAGW.IsChecked, rightAGW.IsChecked);
+            };
+        }
+        private void SetupLineRightClickOptions(PropertyTree tree)
+        {
+            var vec = _ownerline.GetVector();
+            var len = vec.Length;
+            var angle = Angle.FromVector(vec);
+            angle.Degrees += 90;
+
+            var lineProp = tree.Add("Line Properties", 120);
+
+            if (_ownerline.Type != LineType.Scenery)
+            {
+                var id = new NumberProperty(lineProp)
+                {
+                    Min = 0,
+                    Max = int.MaxValue - 1,
+                    NumberValue = _ownerline.ID,
+                    OnlyWholeNumbers = true,
+                    IsDisabled = true
+                };
+                id.ValueChanged += (o, e) =>
+                {
+                    ChangeID((int)id.NumberValue); //TODO: Add the ability to change line IDs
+                };
+                lineProp.Add("ID", id);
+            }
+
+            length = new NumberProperty(lineProp)
+            {
+                Min = 0.0000001,
+                Max = double.MaxValue - 1,
+                NumberValue = len,
+            };
+            length.ValueChanged += (o, e) =>
+            {
+                ChangeLength(length.NumberValue);
+            };
+            lineProp.Add("Length", length);
+
+            angleProp = new NumberProperty(lineProp)
+            {
+                Min = 0,
+                Max = 360,
+                NumberValue = angle.Degrees,
+            };
+            angleProp.ValueChanged += (o, e) =>
+            {
+                ChangeAngle(angleProp.NumberValue);
+            };
+            lineProp.Add("Angle", angleProp);
+
+            if (_ownerline.Type != LineType.Scenery)
+            {
+                SetupBlueAndRedOptions(tree);
             }
             else
             {
-                var table = tree.Add("Scenery Options", 120);
-                var width = new NumberProperty(table)
-                {
-                    Min = 0.1,
-                    Max = 25.5,
-                    NumberValue = _ownerline.Width,
-                };
-                width.ValueChanged += (o, e) =>
-                {
-                    ChangeWidth(width.NumberValue);
-                };
+                SetupSceneryOptions(tree, lineProp);
             }
         }
-
-        private void ChangeWidth(double width)
-        {
-            using (var trk = _editor.CreateTrackWriter())
-            {
-                var cpy = _ownerline.Clone(); 
-                cpy.Width = (float)width;
-                UpdateOwnerLine(trk, cpy);
-            }
-        }
-
         private void UpdateOwnerLine(TrackWriter trk, GameLine replacement)
         {
             UpdateLine(trk, _ownerline, replacement);
@@ -269,6 +339,63 @@ namespace linerider.UI
             trk.ReplaceLine(current, replacement);
             _editor.NotifyTrackChanged();
             _editor.Invalidate();
+        }
+        private void ChangeID(int newID)
+        {
+
+        }
+        private void ChangeAngle(double numberValue)
+        {
+            var multilines = GetMultiLines(false);
+            using (var trk = _editor.CreateTrackWriter())
+            {
+                var cpy = _ownerline.Clone();
+                var correctLength = cpy.GetVector().Length;
+                
+                var newPos = Utility.Rotate(cpy.Position2, cpy.Position, (Angle.FromDegrees(numberValue - 90) - Angle.FromVector(cpy.GetVector())));
+                cpy.Position2 = newPos;
+
+                UpdateOwnerLine(trk, cpy);
+                
+                foreach (var line in multilines)
+                {
+                    var copy = line.Clone();
+                    copy.Position2 = newPos;
+                    UpdateLine(trk, line, copy);
+                }
+            }
+        }
+        private void ChangeLength(double length)
+        {
+            var multilines = GetMultiLines(false);
+            using (var trk = _editor.CreateTrackWriter())
+            {
+                var cpy = _ownerline.Clone();
+                var angle = Angle.FromVector(cpy.GetVector()).Radians;
+
+                var x2 = cpy.Position.X + (length * Math.Cos(angle));
+                var y2 = cpy.Position.Y + (length * Math.Sin(angle));
+                
+                var newPos = new Vector2d(x2, y2);
+                cpy.Position2 = newPos;
+                UpdateOwnerLine(trk, cpy);
+
+                foreach (var line in multilines)
+                {
+                    var copy = line.Clone();
+                    copy.Position2 = newPos;
+                    UpdateLine(trk, line, copy);
+                }
+            }
+        }
+        private void ChangeWidth(double width)
+        {
+            using (var trk = _editor.CreateTrackWriter())
+            {
+                var cpy = _ownerline.Clone();
+                cpy.Width = (float)width;
+                UpdateOwnerLine(trk, cpy);
+            }
         }
         private void ChangeMultiplier(int mul)
         {
@@ -329,8 +456,36 @@ namespace linerider.UI
                 }
             }
         }
+
+        private void UpdateAGWs(bool left, bool right)
+        {
+            StandardLine.Ext newExt = (StandardLine.Ext)((left ? 1 : 0) + (right ? 2 : 0));
+            
+            var multilines = GetMultiLines(false);
+            using (var trk = _editor.CreateTrackWriter())
+            {
+                trk.DisableExtensionUpdating();
+
+                StandardLine cpy = (StandardLine)_ownerline.Clone();
+                cpy.Extension = newExt;
+                UpdateOwnerLine(trk, cpy);
+
+                foreach (var line in multilines)
+                {
+                    StandardLine copy = (StandardLine)line.Clone();
+                    copy.Extension = newExt;
+                    UpdateLine(trk, line, copy);
+                }
+            }
+        }
         private SimulationCell GetMultiLines(bool includeowner)
         {
+            if (_ownerline.Type == LineType.Scenery)
+            {
+                return new SimulationCell();
+            }
+
+
             SimulationCell redlines = new SimulationCell();
             using (var trk = _editor.CreateTrackReader())
             {
